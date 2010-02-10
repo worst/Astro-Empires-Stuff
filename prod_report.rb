2d9bbf5f15e59abc5f579dbb990f1f05cf66c25c
worst <jeremy.blackburn@gmail.com>
2010-02-10 17:38:33 -0500

    Should now work with data from individual production pages too...

--- prod_report.rb
+++ prod_report.rb
@@ -1,23 +1,33 @@
 f = File.open(ARGV[0])
 r = /[aA-zZ ]+([0-9]+)[ ]([aA-zZ]+[ aA-zZ]+) at[ aA-zZ]/
 
+r2 = /Production of ([0-9]+) ([aA-zZ ]+)/i
+
 hash = {}
 f.lines.each do |l|
   matches = l.split("\t")[1].match(r)
   next if matches.nil?
-  
+
   caps = matches.captures
-  if hash[caps[1]].nil?
-    hash[caps[1]] = caps[0].to_i
-  else
-    hash[caps[1]] += caps[0].to_i
-  end
+  hash[caps[1]] ||= 0
+  
+  hash[caps[1]] += caps[0].to_i
 end
 
-f.close
+f.rewind
+
+matches = f.read.scan(r2)
+
+matches.each do |match|
+  hash[match[1]] ||= 0
+
+  hash[match[1]] += match[0].to_i
+  hash[match[1]] += match[0].to_i
+end
+
 puts
 puts "="*50
-puts "Visible production from credits page:"
+puts "Visible production:"
 hash.each_pair {|k, v| puts "#{k}, #{v}"}
 puts "="*50
 puts
