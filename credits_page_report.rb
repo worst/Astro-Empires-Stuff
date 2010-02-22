80f876d6095346bdaab9c704c02381233fcf9bef
worst <jeremy.blackburn@gmail.com>
2010-02-22 15:09:07 -0500

    Added some junk...

--- credits_page_report.rb
+++ credits_page_report.rb
@@ -4,8 +4,7 @@
 f = File.open(ARGV[0])
 r = /[aA-zZ ]+([0-9]+)[ ]([aA-zZ]+[ aA-zZ]+) at[ aA-zZ]/
 
-r2 = /Production of ([0-9]+) ([aA-zZ ]+)/i
-# Production of ([0-9]+) ([aA-zZ ]+)[^at]
+#r2 = /Production of ([0-9]+) ([aA-zZ ]+)/i
 
 hash = {}
 f.lines.each do |l|
@@ -22,13 +21,13 @@ end
 
 f.rewind
 
-matches = f.read.scan(r2)
-
-matches.each do |match|
-  hash[match[1]] ||= 0
-
-  hash[match[1]] += match[0].to_i
-end
+# matches = f.read.scan(r2)
+# 
+# matches.each do |match|
+#   hash[match[1]] ||= 0
+# 
+#   hash[match[1]] += match[0].to_i
+# end
 
 puts
 puts "="*50
